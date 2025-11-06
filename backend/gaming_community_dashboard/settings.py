4a1ba1fec76cb7217c19548e8d2f575948402d76
Luis Almeida <122276206+lra2001@users.noreply.github.com>
2025-11-06 12:18:25 +0000

    Update settings.py
    - Update BASE_DIR
    - Update title on index.html

--- backend/gaming_community_dashboard/settings.py
+++ backend/gaming_community_dashboard/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -64,10 +64,13 @@ ROOT_URLCONF = 'gaming_community_dashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "frontend"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                "django.template.context_processors.debug",
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
